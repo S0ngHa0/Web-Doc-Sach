2fa107423ddf261fae55fb3da0fdaac540e4b731
Axeon <khoabmt12354@gmail.com>
2023-03-28 10:16:25 +0700

    Add style for page

---
+++ Web-Doc-Sach/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Web_Doc_Sach.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Web_Doc_Sach.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Web_Doc_Sach.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
