11e42a3a09c4fb81bb64478f8bfa3c96332b861e
S0ngHa0 <115171734+S0ngHa0@users.noreply.github.com>
2023-03-28 09:25:18 +0700

    Add project files.

---
+++ Web-Doc-Sach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_Doc_Sach.Startup))]
+namespace Web_Doc_Sach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
